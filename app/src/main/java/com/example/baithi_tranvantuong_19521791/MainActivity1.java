43aaefb12a8ed16bfd04380d386c311b49286d8e
tuong3920 <tranvantuongtrang4@gmail.com>
2022-03-21 07:43:32 +0700

    finish

---
+++ app/src/main/java/com/example/baithi_tranvantuong_19521791/MainActivity1.java
@@ -0,0 +1,14 @@
+package com.example.baithi_tranvantuong_19521791;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+}
